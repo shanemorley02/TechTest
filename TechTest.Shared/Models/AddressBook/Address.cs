10f1c957066c58d2068f8dece62af2dc699f1653
Shane.Morley <shanemorley02@gmail.com>
2023-12-19 07:36:42 +0000

    Added Shared project and Tests project

---
+++ TechTest.Shared/Models/AddressBook/Address.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace TechTest.Shared.Models.AddressBook
+{
+    public class Address
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+        [JsonProperty(PropertyName = "first_name")]
+        public string? FirstName { get; set; }
+        [JsonProperty(PropertyName = "last_name")]
+        public string? LastName { get; set; }
+        [JsonProperty(PropertyName = "phone")]
+        public string? Phone { get; set; }
+        [JsonProperty(PropertyName = "email")]
+        public string? Email { get; set; }
+    }
+}
---
+++ TechTest.Tests/AddressBookTests.cs
@@ -0,0 +1,17 @@
+namespace TechTest.Tests
+{
+    [TestFixture]
+    public class AddressBookTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
