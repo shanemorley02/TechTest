66dbcd64f804a3903346c07dc2688746f38ae1f3
Shane.Morley <shanemorley02@gmail.com>
2023-12-19 07:15:54 +0000

    Create API project, removed the boiler plate and added in api methods.

---
+++ TechTest/AddressBookApi.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace TechTest.API
+{
+    public static class AddressBookApi
+    {
+        public static void ConfigureApi(this WebApplication app)
+        {
+            app.MapGet("/AddressBook", GetAllAddresses);
+            app.MapPost("/AddressBook/GetAddress", GetAddress);
+            app.MapPost("/AddressBook", AddAddress);
+            app.MapPut("/AddressBook", UpdateAddress);
+            app.MapDelete("/AddressBook", DeleteAddress);
+        }
+
+        private static async Task<IResult> GetAllAddresses()
+        {
+            return null;
+        }
+
+        private static async Task<IResult> GetAddress()
+        {
+            return null;
+        }
+
+        private static async Task<IResult> AddAddress()
+        {
+            return null;
+        }
+
+        private static async Task<IResult> UpdateAddress()
+        {
+            return null;
+        }
+        private static async Task<IResult> DeleteAddress()
+        {
+            return null;
+        }
+    }
+}
